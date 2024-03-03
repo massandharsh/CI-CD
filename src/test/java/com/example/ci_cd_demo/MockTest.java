01d7ba5fe8aaf909be2c5d547a5eed0dbc620822
Harsh Massand <harsh.massand@sap.com>
2024-03-03 20:35:22 +0530

    Mock Mvc Updated
    Signed-off-by: Harsh Massand <harsh.massand@sap.com>

--- src/test/java/com/example/ci_cd_demo/MockTest.java
+++ src/test/java/com/example/ci_cd_demo/MockTest.java
@@ -2,6 +2,7 @@ package com.example.ci_cd_demo;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
@@ -9,9 +10,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest
-@RequiredArgsConstructor
 public class MockTest {
-    private final MockMvc mockMvc;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void testGreetingEndPoint() throws Exception {
